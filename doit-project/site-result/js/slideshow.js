a711adba761db37417eb18ecd9a6add0a96bfe2f
linuxul <jsseo@eflash.co.kr>
2024-02-07 11:27:14 +0900

    소스 정렬 - cmd + j + f

--- doit-project/site-result/js/slideshow.js
+++ doit-project/site-result/js/slideshow.js
@@ -8,22 +8,22 @@ prev.onclick = prevSlide;
 next.onclick = nextSlide;
 
 function showSlides(n) {
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[n].style.display = "block";
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[n].style.display = "block";
 }
 
 function prevSlide() {
-  if (current > 0) current -= 1;
-  else
-    current = slides.length - 1;
+    if (current > 0) current -= 1;
+    else
+        current = slides.length - 1;
     showSlides(current);
 }
 
 function nextSlide() {
-  if (current < slides.length - 1) current += 1;
-  else
-    current = 0;
-    showSlides(current);  
+    if (current < slides.length - 1) current += 1;
+    else
+        current = 0;
+    showSlides(current);
 }
